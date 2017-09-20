1664b5625979611c1b52b58301fc169ae0f076d9
Bruce McPherson <bruce@mcpher.com>
2017-09-20 13:04:48 +0100

    updated by GasGit automation

--- scripts/curry.js
+++ scripts/curry.js
@@ -34,4 +34,109 @@ function currying() {
   
   var saveCurry = setType ("Script");
   saveCurry ("korma");
+
+  
+  /**
+   * general function to set key/value in given store
+   * @param {string} store script, document or user
+   * @param {string} key the key
+   * @param {number|string|boolean} the value to set
+   * @return {PropertiesStore} the store used
+   */
+  function setPropertyValue (store , key , value) {
+    var store = PropertiesService[
+      'get' + 
+      store.slice (0,1).toUpperCase() + 
+      store.slice(1).toLowerCase() + 
+      "Properties"
+    ]();
+    store.setProperty (key , value);
+    return store;
+  }
+  
+  // get curry from cUseful library
+  var curry = cUseful.Utils.curry;
+  
+  // all of these work
+  setPropertyValue ("script" , "preferredVariety" , "biryani");
+  curry (setPropertyValue, "script") ( "preferredVariety" , "biryani");
+  curry (setPropertyValue) ("script" , "preferredVariety" , "biryani");
+  curry (setPropertyValue) ("script") ( "preferredVariety" , "biryani");
+  curry (setPropertyValue) ("script", "preferredVariety" ) ("biryani");
+  curry (setPropertyValue) ("script", "preferredVariety" , "biryani");
+  curry (setPropertyValue, "script", "preferredVariety" )( "biryani");
+  
+  // but  becomes useful if we do this
+  var setCurriedScript = curry (setPropertyValue , "script");
+  setCurriedScript ("preferredVariety" , "bhuna");
+  
+  // even more
+  var setPreferred = curry (setPropertyValue , "script","preferredVariety");
+  setPreferred ( "korai");
+  
+  // or this
+  var setPreferred = setCurriedScript("preferredVariety");
+  setPreferred ( "dhansak");
+  
+  // so now we'll change the underlying script to be more useful
+  /**
+   * general function to set key/value in given store
+   * @param {string} store script, document or user
+   * @param {string} key the key
+   * @param {*} the value to set
+   * @return {PropertiesStore} the store used
+   */
+  function setProperty (store , key , value) {
+    var store = PropertiesService[
+      'get' + 
+      store.slice (0,1).toUpperCase() + 
+      store.slice(1).toLowerCase() + 
+      "Properties"
+    ]();
+    // maybe it's an object
+    if (typeof value === "object") {
+      value = JSON.stringify (value);
+    }
+    // and might as well exponential back off it
+    cUseful.Utils.expBackoff ( function () {
+      return store.setProperty (key , value);
+    });
+    
+    return store;
+  }
+  
+  //now curry that
+  var setFavorite = curry (setProperty , "script" , "favorite");
+  setFavorite ({
+    name:"vindaloo",
+    spices:[
+      "coriander", 
+      "cumin", 
+      "cinnamon", 
+      "mustard", 
+      "cayenne", 
+      "cardamom", 
+      "turmeric", 
+      "black pepper" ,
+      "cloves"
+      ]
+  });
+  
+  // handy to pass to a library so the library doesn't need to get property permissions
+  MyLib.setSpices (curry (setProperty , "script" , "favorite") , "dhanask" , ["turmeric", "cinammon", "cardamom"]);
+  
 }
+
+var MyLib = (function (ns) {
+
+  ns.setSpices = function (setFavorite , name , spices) {
+    return setFavorite ( {
+      name:name,
+      spices:spices
+    });
+    
+  };
+  return ns;
+})({});
+
+
